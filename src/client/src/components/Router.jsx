8f6b7b4aeaace28f4843359c222b5ec393037b8f
Tristen <janzentristen@yahoo.com>
2021-04-27 17:30:08 -0500

    Made a Choice component, a logout component, and added a logout bar.
    The choice component is for between login and view/managing, the logout component simply says thank you for using it and offers a login button. The logout bar simply takes you to the logout page.

--- src/client/src/components/Router.jsx
+++ src/client/src/components/Router.jsx
@@ -4,20 +4,46 @@ import FlashcardCreate from './FlashcardCreate'
 import FlashcardEdit from './FlashcardEdit'
 import Manage from './Manage'
 import App from './App.jsx'
+import { withRouter} from 'react-router-dom';
 import ViewCardsComponent from './ViewCardsComponent.jsx'
+import Choice from './Choice'
+import Logout from './Logout'
+import {Menu} from 'antd'
+import {AppstoreOutlined} from '@ant-design/icons';
+const { SubMenu } = Menu;
 
 class RouterComponent extends Component {
+    handleClick(){
+        window.location.href = "http://localhost:3000/logout";
+      };
    render() {
+       let record = 1;
        return(
            <div>
             <Router>
+            <Menu
+                onClick={this.handleClick}
+                style={{ width: 256 }}
+                mode="inline">
+
+<SubMenu key="sub1" icon={<AppstoreOutlined />} title="Profile">
+          <Menu.Item key="5">Logout</Menu.Item>
+ </SubMenu>
+
+
+
+
+
+                </Menu>
+
                     <Switch>
                         <Route exact path="/"><App/></Route>
-                        <Route exact path="/manage"><Manage to={{state:{user_id:1}}}/></Route>
+                        <Route exact path="/manage"><Manage /></Route>
+                        <Route exact path="/choice"><Choice to={{state: record}} /></Route>
                         <Route exact path="/view"><ViewCardsComponent/></Route>
                         <Route exact path="/flashcards/edit/:id" component={FlashcardEdit} />
                         <Route exact path="/flashcards/create/:id" component={FlashcardCreate} />
-                        <Route exact path="/logout"></Route>
+                        <Route exact path="/logout"><Logout/></Route>
                     </Switch>
             </ Router>
             </div>
